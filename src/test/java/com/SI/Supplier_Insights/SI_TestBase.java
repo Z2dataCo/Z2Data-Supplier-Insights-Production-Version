906120e998ed6b32a8335f82e24ef287325dbb92
zayatoo <ahmedzayat2311@gmail.com>
2020-12-08 13:24:48 +0200

    AZ:08122020|1:24PM Updating Pom without Suit

--- src/test/java/com/SI/Supplier_Insights/SI_TestBase.java
+++ src/test/java/com/SI/Supplier_Insights/SI_TestBase.java
@@ -1,5 +1,6 @@
 package com.SI.Supplier_Insights;
 
+
 import com.shaft.gui.browser.BrowserActions;
 import com.shaft.gui.browser.BrowserFactory;
 import org.openqa.selenium.WebDriver;
@@ -7,12 +8,13 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class SI_TestBase {
-    WebDriver driver;
+    public WebDriver driver;
 
     @BeforeSuite
     public void StartDriver() {
         driver = BrowserFactory.getBrowser(BrowserFactory.BrowserType.GOOGLE_CHROME);
-        BrowserActions.navigateToURL(driver, "https://suppliers.z2data.com/","https://suppliers.z2data.com/");
+        BrowserActions.navigateToURL(driver,"https://suppliers.z2data.com/");
+      //  driver.navigate().to("https://suppliers.z2data.com/");
     }
 
 }
