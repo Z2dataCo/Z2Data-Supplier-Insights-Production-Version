f569a3a3792fdbc2ed3d6a1250a61dff2e90e12f
zayatoo <ahmedzayat2311@gmail.com>
2021-03-28 17:04:10 +0200

    AZ2832021|5:03PM Updating Project All Working

--- src/main/java/com/SI/Supplier_Insights/Browse_By_Industry_Page.java
+++ src/main/java/com/SI/Supplier_Insights/Browse_By_Industry_Page.java
@@ -15,7 +15,7 @@ public class Browse_By_Industry_Page {
     public By Product_Name = By.xpath("//a[@ng-reflect-router-link='/Company/1291160']");
     public By Industry = By.partialLinkText("3D Printing & Additi");
     public By CheckBox_Supplier = By.xpath("//tbody/tr[1]/td[1]/input[1]");
-    public By Save_Btn = By.xpath("//*[@id=\"z2tableA-contain\"]/div/div[2]/div[1]/div[1]/a[3]");
+    public By Save_Btn = By.xpath("//a[@class='btn btn-outline-z2 float-left mr-03'][normalize-space()='Save']");
 
     public void Z2D_Click_On_Industry() {
         ElementActions.click(driver, Industry);
